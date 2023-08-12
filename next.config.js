947743f008228b5e4c6ee538e18ad72c0e266e56
Adams Abdulmaleek <harbdulmaleek@gmail.com>
2023-08-12 08:56:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,11 +6,9 @@ const nextConfig = {
           source: '/api/:path*',
           destination:
           process.env.NODE_ENV === 'development'
-              ? 'http://127.0.0.1:5000/api/:path*'
-              : '/api/',
+              ? 'https://oc-predictor.onrender.com/api/index'
+              : 'https://oc-predictor.onrender.com/api/index',
       },
       ]
 },
 }
-
-module.exports = nextConfig
